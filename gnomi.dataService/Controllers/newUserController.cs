86c00c3290b2b31373718a5c226dd6edf3dc0953
Brandon Pauly <brandon.m.pauly@gmail.com>
2019-07-22 22:25:20 -0500

    initial commit; added gnomi.models and getting new user data

---
+++ gnomi.dataService/Controllers/newUserController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using gnomi.dataService.requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gnomi.dataService.Controllers
+{
+    [Route("api/newUser")]
+    [ApiController]
+    public class newUserController : ControllerBase
+    {
+        // POST: api/newUser
+        [HttpPost]
+        public void Post([FromBody] newUserRequest data)
+        {
+            var check = data;
+        }
+    }
+}
